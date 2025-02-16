5732297c88824500827e678624ddeb6ed0d583b9
Rohan Palanikumar <palanire@mail.uc.edu>
2025-02-16 14:04:49 -0500

    Add spacing input

--- src/tests/nvidia_cuopt_integration.py
+++ src/tests/nvidia_cuopt_integration.py
@@ -6,7 +6,29 @@ import random
 import shapely.plotting
 from shapely.geometry import Polygon, Point
 
-def create_points_in_polygon(polygon, num_points):
+path = 'C:/Users/rohan/OneDrive - University of Cincinnati/UAV Design/preflight_post_processing_app/src/tests'
+
+def latlon_to_ecef(lat_deg, lon_deg, alt_m=0):
+    """
+    Converts latitude, longitude (in degrees), and altitude (in meters) to 
+    ECEF coordinates.
+    """
+    # WGS 84 ellipsoid parameters
+    a = 6378137.0          # semi-major axis (equatorial radius in meters)
+    e2 = 0.00669438002290  # eccentricity squared
+
+    lat_rad = np.radians(lat_deg)
+    lon_rad = np.radians(lon_deg)
+
+    N = a / np.sqrt(1 - e2 * np.sin(lat_rad)**2)
+
+    x = (N + alt_m) * np.cos(lat_rad) * np.cos(lon_rad)
+    y = (N + alt_m) * np.cos(lat_rad) * np.sin(lon_rad)
+    z = ((1 - e2) * N + alt_m) * np.sin(lat_rad)
+    
+    return x, y, z
+
+def create_points_in_polygon(polygon, spacing, altitude):
     """
     Generates random points within a given polygon.
 
@@ -19,8 +41,15 @@ def create_points_in_polygon(polygon, num_points):
     """
     minx, miny, maxx, maxy = polygon.bounds
 
-    xrange = np.linspace(minx, maxx, num=num_points)
-    yrange = np.linspace(miny, maxy, num=num_points)
+    x1,y1,z1 = latlon_to_ecef(minx, miny, altitude)
+    x2,y2,z2 = latlon_to_ecef(maxx, maxy, altitude)
+
+    dist = np.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
+    num_points = dist / spacing
+    print(num_points)
+
+    xrange = np.linspace(minx, maxx, num=round(num_points))
+    yrange = np.linspace(miny, maxy, num=round(num_points))
     points = []
     while len(points) < num_points:
         for x in xrange:
@@ -31,15 +60,17 @@ def create_points_in_polygon(polygon, num_points):
 
     return points
 
-kml_file = gp.read_file('test_data.kml', layer='QGroundControl Plan KML')
+kml_file = gp.read_file(f'{path}/test_data.kml', layer='QGroundControl Plan KML')
+height = 4.5 # meters
+altitude = np.array(kml_file['geometry'][0].coords)[0][2] + height # meters
 # kml_file.to_file('test_data.shp')
 # kml_file.to_csv('test_data.csv')
-shp_file = gp.read_file('test_data.shp')
+# shp_file = gp.read_file('test_data.shp')
 
 boundary_polygon = kml_file["geometry"][1]
 
-num_points = 75
-points = create_points_in_polygon(boundary_polygon, num_points)
+spacing = 10 # meters
+points = create_points_in_polygon(boundary_polygon, spacing, altitude)
 point_cloud = shapely.plotting.plot_points(points)
 boundary_polygon_line = shapely.plotting.plot_polygon(boundary_polygon)
 
